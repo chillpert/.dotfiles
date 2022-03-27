d187b72fd55ed540d6877019858bf3219989b88e
chillpert <dev@christianhilpert.com>
2022-03-27 15:32:53 +0200

    Delete scripts and clean up i3 config

--- .scripts/dmenu/search_web.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-term=$(printf '' | dmenu -i -fn 'VL PGothic-11.5' -p 'Search the web:') "$@" || exit
-
-term=${term//[ ]/+}
-
-firefox https://www.startpage.com/sp/search?query=$term https://lite.qwant.com/?q=$term https://searx.be/search?q=$term
