a735a4aa14dfcb1899b98b1da5c297d4188150a2
chillpert <dev@christianhilpert.com>
2022-03-30 11:51:14 +0200

    Improve bar and wm display configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q bar_left -c ~/.config/polybar/config.ini &
-polybar -q bar_right -c ~/.config/polybar/config.ini &
+monitor_count=$(polybar -M | wc -l)
+
+# Launch bar(s) depending on monitor count
+if [ "$monitor_count" -eq "1" ]; then
+	echo "Single monitor detected."
+
+	polybar -q bar_left -c ~/.config/polybar/config.ini &
+elif [ "$monitor_count" -gt "1" ]; then
+	echo "Dual monitor detected."
+
+	polybar -q bar_left -c ~/.config/polybar/config.ini &
+	polybar -q bar_right -c ~/.config/polybar/config.ini &
+else
+	echo "Monitor configuration not supported."
+fi
+
