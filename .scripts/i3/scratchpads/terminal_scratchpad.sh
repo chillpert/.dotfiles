37d95bbff39016cedba74552a9ba74f1bb184b89
chillpert <dev@christianhilpert.com>
2022-07-08 20:40:15 +0200

    Switch tabbed with tmux

--- .scripts/i3/scratchpads/terminal_scratchpad.sh
+++ .scripts/i3/scratchpads/terminal_scratchpad.sh
@@ -5,5 +5,5 @@ output=$(ps ax)
 name=terminalScratchpadProcess
 
 if [[ $output != *"$name"* ]]; then
-	tabbed -n $name -r 2 -t '#AF87FF' -o '#26292c' alacritty --embed ''
+    alacritty --class $name -e tmux
 fi
