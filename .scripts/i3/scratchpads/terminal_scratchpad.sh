41fe87d0e560a97627d7344810492580e8dddbdb
chillpert <dev@christianhilpert.com>
2022-07-10 22:14:23 +0200

    Revert "Switch tabbed with tmux"
    This reverts commit 37d95bbff39016cedba74552a9ba74f1bb184b89.

--- .scripts/i3/scratchpads/terminal_scratchpad.sh
+++ .scripts/i3/scratchpads/terminal_scratchpad.sh
@@ -5,5 +5,5 @@ output=$(ps ax)
 name=terminalScratchpadProcess
 
 if [[ $output != *"$name"* ]]; then
-    alacritty --class $name -e tmux
+	tabbed -n $name -r 2 -t '#AF87FF' -o '#26292c' alacritty --embed ''
 fi
