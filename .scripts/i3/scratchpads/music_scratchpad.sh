098c114025dc8276b0e0593b945f500e6e5a319e
chillpert <dev@christianhilpert.com>
2022-07-14 20:47:44 +0200

    Switch spotify-tui with ncspot

--- .scripts/i3/scratchpads/music_scratchpad.sh
+++ .scripts/i3/scratchpads/music_scratchpad.sh
@@ -5,9 +5,11 @@ output=$(ps ax)
 name=musicScratchpadProcess
 
 if [[ $output != *"$name"* ]]; then
-    if [ ! $(pgrep "spotifyd") ]; then
-        spotifyd
-    fi
+    # if [ ! $(pgrep "spotifyd") ]; then
+    #     spotifyd
+    # fi
+	#
+	# alacritty --class $name -e spt
 
-	alacritty --class $name -e spt
+    alacritty --class $name -e ncspot
 fi
