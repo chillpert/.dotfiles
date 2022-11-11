080e5dd562b2cff656db5907e4419bc5e59b2603
chillpert <dev@christianhilpert.com>
2022-11-11 12:16:06 +0100

    Fix nvim-cmp deprecated function call

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -115,7 +115,7 @@ cmp.setup({
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- bash
 require 'lspconfig'.bashls.setup {}
