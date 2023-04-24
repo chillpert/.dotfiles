5b63bcc6845e9eab806c7e319a9dedeafc2988e4
chillpert <dev@christianhilpert.com>
2023-04-24 15:02:21 +0200

    Fix dmenu timer

--- .scripts/dmenu/timer.sh
+++ .scripts/dmenu/timer.sh
@@ -4,7 +4,7 @@ minutes=$(printf '' | dmenu -i -fn 'VL PGothic-11.5' -p 'Timer (Minutes)') "$@" 
 
 sleep ${minutes}m
 # Only play alarm for 10 seconds
-timeout 14 mpv --quiet --ytdl=no ~/.scripts/dmenu/free-software-song.au --start=00:00 &
+timeout 14 mpv --quiet --ytdl=no ~/.scripts/dmenu/free-software-song.au --start=00:00 --no-terminal &
 
 # Some visual feedback
 xrandr-invert-colors
