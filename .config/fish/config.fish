71bf09dad42bb2ee1907978448801414b5446d7c
chillpert <dev@christianhilpert.com>
2026-05-16 17:57:04 +0200

    Add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
